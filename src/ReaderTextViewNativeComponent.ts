e1d6629e55d8288ccdd751d502f9a180e57a5508
Bilal <bahma060@gmail.com>
2026-05-26 08:22:23 -0400

    Fix ReaderText Fabric codegen export

--- src/ReaderTextViewNativeComponent.ts
+++ src/ReaderTextViewNativeComponent.ts
@@ -1,4 +1,4 @@
-import { Platform, requireNativeComponent, type HostComponent, type ViewProps } from 'react-native';
+import type { HostComponent, ViewProps } from 'react-native';
 import type {
   DirectEventHandler,
   Double,
@@ -119,8 +119,6 @@ export interface NativeProps extends ViewProps {
   onContentSizeChange?: DirectEventHandler<NativeContentSizeChangeEvent>;
 }
 
-const NativeReaderTextView = Platform.OS === 'android'
-  ? requireNativeComponent<NativeProps>('ReaderTextView')
-  : codegenNativeComponent<NativeProps>('ReaderTextView') as HostComponent<NativeProps>;
-
-export default NativeReaderTextView;
+export default codegenNativeComponent<NativeProps>(
+  'ReaderTextView',
+) as HostComponent<NativeProps>;
